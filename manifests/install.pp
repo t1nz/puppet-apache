19e6c4c71163f5912c778986bc0b289fad9fc5a1
root <root@e15647cd181c.mylabserver.com>
2021-11-23 23:58:29 +0000

    Refractored module using Hiera.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,10 +2,8 @@
 #   This will install base Apache Package
 #   
 class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+) {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
